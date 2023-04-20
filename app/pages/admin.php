1cca3ddbfc0cb6a364f068553cc6cba84b03a605
santimcs <santimcs@hotmail.com>
2023-04-20 16:38:51 +0700

    23-04-20 01

---
+++ app/pages/admin.php
@@ -0,0 +1,40 @@
+<?php 
+
+if(!is_admin())
+{
+	message("only admins can access the admin page");
+	redirect('login');
+}
+
+	// $section 	= $URL[1] ?? "dashboard";
+	$section 	= isset($URL[1]) ? $URL[1] : "dashboard";
+	// $action 	= $URL[2] ?? null;
+	$action 	= isset($URL[2]) ? $URL[2] : null;
+	// $id 		= $URL[3] ?? null;
+	$id 		= isset($URL[3]) ? $URL[3] : null;
+
+	switch ($section) {
+		case 'dashboard':
+			require page('admin/dashboard');
+			break;
+
+		case 'users':
+			require page('admin/users');
+			break;
+
+		case 'categories':
+			require page('admin/categories');
+			break;
+
+		case 'artists':
+			require page('admin/artists');
+			break;
+
+		case 'songs':
+			require page('admin/songs');
+			break;
+		
+		default:
+			require page('admin/404');
+			break;
+	}
